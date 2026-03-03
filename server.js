5f8989226da8fb8c45e595bb21770ec3cf0d5afb
Mooo282 <mohpro1122@hotmail.com>
2026-03-04 00:57:24 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -167,4 +167,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server Live` || [Node.js Docs](https://nodejs.org)));
+server.listen(PORT, () => console.log(`Server Online on port ${PORT}`));
